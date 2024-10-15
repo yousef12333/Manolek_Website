16b7510fdc90366a986ba4c55b3e81d32401a711
Youssef <s129059@ap.be>
2024-10-15 11:14:39 +0200

    Added text square component

--- src/app/components/electrical/electrical.component.ts
+++ src/app/components/electrical/electrical.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { PageBannerComponent } from "../page-banner/page-banner.component";
+import { SquareComponent } from "../square/square.component";
 
 @Component({
   selector: 'app-electrical',
   standalone: true,
-  imports: [PageBannerComponent],
+  imports: [PageBannerComponent, SquareComponent],
   templateUrl: './electrical.component.html',
   styleUrl: './electrical.component.css'
 })

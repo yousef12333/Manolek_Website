cc9efef346e8205c732acb098e93a73bfe55d345
Youssef <s129059@ap.be>
2024-10-21 00:02:18 +0200

    Finish this feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { FormsModule } from '@angular/forms';
+import { ParallaxContactComponent } from "./components/parallax-contact/parallax-contact.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLinkActive, RouterLink, HeaderComponent, FooterComponent, FormsModule],
+  imports: [RouterOutlet, RouterLinkActive, RouterLink, HeaderComponent, FooterComponent, FormsModule, ParallaxContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

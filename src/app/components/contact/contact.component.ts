dd4ede8c0d72672d30fcb72e770060f2984f202d
Youssef <s129059@ap.be>
2024-10-21 18:27:12 +0200

    Map working fine with marker and small company name

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { PageBannerComponent } from "../page-banner/page-banner.component";
 import { FooterComponent } from "../footer/footer.component";
 import { SquareComponent } from "../square/square.component";
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-contact',
@@ -11,5 +12,22 @@ import { SquareComponent } from "../square/square.component";
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  ngOnInit(): void{
+    var map = L.map('map').setView([51.2019702, 4.3908187], 15);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+  var marker = L.marker([51.2019702, 4.3908187]).addTo(map);
+  var title = L.marker([51.2019702, 4.3908187], {
+    icon: L.divIcon({
+        className: 'text-below-marker',
+        html: "Manolek&nbsp;(BV)",
+        iconSize: [400, 40],
+        iconAnchor: [30, 2] 
+    })
+}).addTo(map);
 
+}
+  
 }

0a842f84b48a1a3d8c9cc51bf4306ecb68d698e2
Youssef <s129059@ap.be>
2024-10-22 19:48:23 +0200

    working slider without css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { FormsModule } from '@angular/forms';
 import { ParallaxContactComponent } from "./components/parallax-contact/parallax-contact.component";
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLinkActive, RouterLink, HeaderComponent, FooterComponent, FormsModule, ParallaxContactComponent],
+  imports: [RouterOutlet, RouterLinkActive, RouterLink, HeaderComponent, FooterComponent, FormsModule, ParallaxContactComponent, RouterModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
-import { PageBannerComponent } from "../page-banner/page-banner.component";
-import { FrontpageBannerComponent } from "../frontpage-banner/frontpage-banner.component";
-import { ParallaxContactComponent } from "../parallax-contact/parallax-contact.component";
-import {Router, RouterLink} from "@angular/router";
+import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { register } from 'swiper/element/bundle';
+import { PageBannerComponent } from '../page-banner/page-banner.component';
+import { FrontpageBannerComponent } from '../frontpage-banner/frontpage-banner.component';
+import { ParallaxContactComponent } from '../parallax-contact/parallax-contact.component';
+
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [RouterLink, PageBannerComponent, FrontpageBannerComponent, ParallaxContactComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class HomeComponent {
-  }
+export class HomeComponent implements OnInit {
+  constructor() { }
 
+  ngOnInit(): void {
+    register(); 
+  }
+}

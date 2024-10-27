0ed1fc6300e0f75d4cb0f7eff775a8fda6e9c62e
Youssef <s129059@ap.be>
2024-10-27 18:02:10 +0100

    Fixed slider speed bug

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterViewInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { register } from 'swiper/element/bundle';
 import { PageBannerComponent } from '../page-banner/page-banner.component';
@@ -13,21 +13,39 @@ import { ParallaxContactComponent } from '../parallax-contact/parallax-contact.c
   styleUrls: ['./home.component.css'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
+  private autoSlideInterval: any;
+
   constructor() { }
 
   ngOnInit(): void {
     register(); 
   }
-  
+
   ngAfterViewInit(): void {
-    setInterval(() => {
-      const swiper: any = document.querySelector('swiper-container');
-      if (swiper && swiper.swiper) {
-        swiper.swiper.slideNext();
-      }
-    }, 7000); 
+    const swiper: any = document.querySelector('swiper-container');
+
+    if (swiper && swiper.swiper) {
+      this.startAutoSlide(swiper);
+    }
+  }
+
+  private startAutoSlide(swiper: any): void {
+    this.clearAutoSlide(); 
+
+    this.autoSlideInterval = setInterval(() => {
+      swiper.swiper.slideNext();
+    }, 10000);
+  }
+
+  private clearAutoSlide(): void {
+    if (this.autoSlideInterval) {
+      clearInterval(this.autoSlideInterval);
+      this.autoSlideInterval = null;
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.clearAutoSlide();
   }
 }
-
- 
